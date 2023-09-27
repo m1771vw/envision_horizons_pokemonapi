39dd18d3d1f82ce03dc9862a1a7ba283214f0cbf
William Yang <williamyang93@gmail.com>
2023-09-27 14:36:55 -0700

    first commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace envision_horizons_pokemonapi.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
